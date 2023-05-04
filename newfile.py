a66ba78ac7ac38ca7e8b25f518d858326735e6ff
BLACK DEMON <132496107+black-demon-dr7@users.noreply.github.com>
2023-05-04 20:53:06 +0300

    Add files via upload

--- newfile.py
+++ newfile.py
@@ -5,6 +5,8 @@ import time
 import datetime
 import random
 import compileall
+from time import sleep
+import webbrowser,pyfiglet
 #start
 c = ['\033[1;30m','\033[1;31m','\033[1;32m','\033[1;33m','\033[1;34m','\033[1;35m','\033[1;36m','\033[1;37m']
 def jalan(z):
@@ -16,31 +18,37 @@ os.system('clear' )
 os.system('clear') 
 print(random.choice(c))
 #__________________________________________________________________________
+time.sleep(0.4)
+print('\033[33m[\033[36m ◕‿◕ \033[33m] \033[1;36mWelcom In My Tool')
+time.sleep(0.5)
 print ("""  
-  ______   _______ ___ ___ _______ ______  
- |   _  \ |   _   |   Y   |   _   |   _  \ 
- |.  |   \|.  1___|.      |.  |   |.  |   |
- |.  |    |.  __)_|. \_/  |.  |   |.  |   |
- |:  1    |:  1   |:  |   |:  1   |:  |   |
- |::.. . /|::.. . |::.|:. |::.. . |::.|   |
- `------' `-------`--- ---`-------`--- ---'                            by : >> Black Demon                                       
+ (           *       )     )  
+ )\ )      (  `   ( /(  ( /(  
+(()/(  (   )\))(  )\()) )\()) 
+ /(_)) )\ ((_)()\((_)\ ((_)\  
+(_))_ ((_)(_()((_) ((_) _((_) 
+ |   \| __|  \/  |/ _ \| \| | 
+ | |) | _|| |\/| | (_) | .` | 
+ |___/|___|_|  |_|\___/|_|\_| 
+                                                       by : >> Black Demon                                       
 """)
+time.sleep(0.6)
 
 def main():
     print('')
-    print("\033[1;31m[01] \033[36mHack with Phishing Page")
+    print("\033[33m[\033[36m 01 \033[33m] \033[1;36mHack with Phishing Page")
     time.sleep(0.3)
 #________________
-    print('\033[1;31m[02] \033[36mHack With gust ')
+    print('\033[33m[\033[36m 02 \033[33m] \033[1;36mHack With gust ')
     time.sleep(0.3)
 #__________________________________________
-    print('\033[1;31m[03] \033[36mAbout ')
+    print('\033[33m[\033[36m 03 \033[33m] \033[1;36mAbout ')
     time.sleep(0.3)
 #_________________________________________________________________________
-    print('\033[1;31m[04] \033[36mExit ')
+    print('\033[33m[\033[36m 04 \033[33m] \033[1;36mExit ')
     time.sleep(0.3)
 main()
-choose = input("\033[1;37mChoose an option : ")
+choose = input("\033[33mChoose an option :\033[36m  ")
 time.sleep(0.3)
 
 #commands
@@ -48,13 +56,17 @@ time.sleep(0.3)
 if choose ==  '1':
     os.system('bash install.sh')
     os.system('bash demon.sh')
+if choose == '2':
+	print ('7')
 if choose ==  '3':
-    	print(''' 
-Welcome To My Tool
+    	print('''
+\033[35mWelcome To My Tool
 	
-Tool Dev By : >> Black Demon
+\033[33mTool Dev By : >> \033[31mBlack Demon
 
-My Channel : »» http://t.me/BLACK_DEMON_VX ''')
-
+\033[37mMy Channel : »» \033[34mhttp://t.me/BLACK_DEMON_VX ''')
+sleep(0.001/0.02)               
+webbrowser.open('https://t.me/black_code_22')
+print('')
 if choose == '4':
 	os.system('clear')
